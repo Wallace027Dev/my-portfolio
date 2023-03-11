f5cc4f36900c2b3f9bc80ca8d6276a527aa10ff6
Wallace Vieira <wallaceofc@hotmail.com>
2023-03-11 18:37:06 -0300

    feat: MyExp refatorado

--- src/components/MyExp.js
+++ src/components/MyExp.js
@@ -2,22 +2,60 @@ import designIcon from "../images/DesignIcon.svg";
 import projectIcons from "../images/ProjectsIcon.svg";
 import codeIcon from "../images/CodeIcon.svg";
 import devCount from "../utils/devCount";
+import styled from "styled-components";
+
+const Exp = styled.section`
+	.my-exp, .my-job {
+		flex-direction: column;
+		border-radius: 1rem;
+	}
+	.exp-span {
+		gap: 5rem;
+		div {
+			flex-direction: column;
+			border: 1px solid var(--primary);
+			border-radius: 1rem;
+			width: 15rem;
+			height: 12.5rem;
+		}
+	}
+
+	.my-exp p {
+		font-size: 1rem;
+		}
+
+	.my-exp h2 {
+		font-size: 2rem;
+		}
+		
+	.my-job {
+		margin-top: 2rem;
+		width: 56.25rem;
+		height: 5rem;
+		color: var(--primary);
+		background-color: var(--secondary);
+	}
+
+	.my-job h2 {
+		letter-spacing: 1rem;
+	}
+`
 
 export default function MyExp() {
   return (
-    <section id="my-exp" className="my-exp">
+    <Exp id="my-exp" className="my-exp">
       <span className="exp-span center">
-        <div>
+        <div className="center">
           <img src={codeIcon} alt="Ícone de código" />
           <p>{devCount()} anos estudando</p>
           <h2>Programação</h2>
         </div>
-        <div>
+        <div className="center">
           <img src={projectIcons} alt="Ícone de Gráficos" />
           <p>Planejando transição de</p>
           <h2>Carreira</h2>
         </div>
-        <div>
+        <div className="center">
           <img src={designIcon} alt="Ícone de design" />
           <p>Análise e Desenvolvimento de</p>
           <h2>Sistemas</h2>
@@ -27,6 +65,6 @@ export default function MyExp() {
         <p>DESENVOLVEDOR</p>
         <h2>FULLSTACK</h2>
       </div>
-    </section>
+    </Exp>
   );
 }
--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -3,52 +3,59 @@ import todoPrint from "../images/projectThumbs/todoPrint.png";
 import postsApi from "../images/projectThumbs/postsApi.png";
 import ProjectModule from "../components/ProjectModule";
 import emBreve from "../images/EmBreve.png";
+import styled from "styled-components";
+
+const Projects = styled.section`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-column-gap: 3rem;
+  grid-row-gap: 3rem;
+  margin-top: 6.25rem;
+`;
 
 export default function MyProjects() {
   return (
-    <section id="my-projects" className="my-projects">
-      <div>
-        <ProjectModule
-          projectName="Autenticação e Armazenamento"
-          subtitle="Nesse projeto foi usado React, Routers e Firebase. Foi um exercício para fixar ferramentas do React e aplicar o Firebase em um projeto."
-          skill1="Firebase"
-          skill2="React JS"
-          skill3="Javascript"
-          imageProject={authStorage}
-          acess="Autenticação e Armazenamento"
-          linkProject="https://autenticacao-e-armazenamento-react.vercel.app/login"
-        />
-        <ProjectModule
-          projectName="To Do List"
-          subtitle="Uma lista de tarefas desenvolvida com JS puro, manipulando o DOM e Eventos(). No projeto foi usado o pré-processador Tailwind para estilização e transpilado com Babel."
-          skill1="Javascript"
-          skill2="Babel"
-          skill3="Tailwind"
-          imageProject={todoPrint}
-          acess="To Do List"
-          linkProject="https://to-do-olive-six.vercel.app/"
-        />
-        <ProjectModule
-          projectName="Media Social"
-          subtitle="Projeto de media social, onde posts são sumbetidos, e, enviados e salvos em uma API. Por enquanto essa API só funciona quando executada em uma aplicação local, como explicado no repositório desse projeto no Github. Mas em breve estou adicionando um banco de dados funcional ao projeto."
-          skill1="Node"
-          skill2="React JS"
-          skill3="Fetch API"
-          imageProject={postsApi}
-          acess="Social Media"
-          linkProject="https://consumo-de-api-com-react.vercel.app/"
-        />
-        <ProjectModule
-          projectName="Projeto vazio"
-          subtitle=" Descrição do meu projeto futuro. Aqui vai ser colocado meu exemplo."
-          skill1="Typescript"
-          skill2="React JS"
-          skill3="Tailwind"
-          imageProject={emBreve}
-          acess="Projeto de tal"
-          linkProject="exemploLink"
-        />
-      </div>
-    </section>
+    <Projects id="my-projects" className="my-projects">
+      <ProjectModule
+        projectName="Autenticação e Armazenamento"
+        subtitle="Nesse projeto foi usado React, Routers e Firebase. Foi um exercício para fixar ferramentas do React e aplicar o Firebase em um projeto."
+        skill1="Firebase"
+        skill2="React JS"
+        skill3="Javascript"
+        imageProject={authStorage}
+        acess="Autenticação e Armazenamento"
+        linkProject="https://autenticacao-e-armazenamento-react.vercel.app/login"
+      />
+      <ProjectModule
+        projectName="To Do List"
+        subtitle="Uma lista de tarefas desenvolvida com JS puro, manipulando o DOM e Eventos(). No projeto foi usado o pré-processador Tailwind para estilização e transpilado com Babel."
+        skill1="Javascript"
+        skill2="Babel"
+        skill3="Tailwind"
+        imageProject={todoPrint}
+        acess="To Do List"
+        linkProject="https://to-do-olive-six.vercel.app/"
+      />
+      <ProjectModule
+        projectName="Media Social"
+        subtitle="Projeto de media social, onde posts são sumbetidos, e, enviados e salvos em uma API. Por enquanto essa API só funciona quando executada em uma aplicação local, como explicado no repositório desse projeto no Github. Mas em breve estou adicionando um banco de dados funcional ao projeto."
+        skill1="Node"
+        skill2="React JS"
+        skill3="Fetch API"
+        imageProject={postsApi}
+        acess="Social Media"
+        linkProject="https://consumo-de-api-com-react.vercel.app/"
+      />
+      <ProjectModule
+        projectName="Projeto vazio"
+        subtitle=" Descrição do meu projeto futuro. Aqui vai ser colocado meu exemplo."
+        skill1="Typescript"
+        skill2="React JS"
+        skill3="Tailwind"
+        imageProject={emBreve}
+        acess="Projeto de tal"
+        linkProject="exemploLink"
+      />
+    </Projects>
   );
 }
