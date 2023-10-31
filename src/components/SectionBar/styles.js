5b3531ccfbbd9ad7411b9add86ca6f648ed7e02d
Wallace Vieira <wallaceofc@hotmail.com>
2023-10-31 15:50:12 -0300

    refactor: project slider; SectionBar styles

---
+++ src/components/SectionBar/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const Container = styled.h2`
+  @keyframes wave {
+    0% {
+      transform: rotate(0deg) scale(1);
+    }
+    15% {
+      transform: rotate(20deg) scale(1.05);
+    }
+    30% {
+      transform: rotate(-20deg) scale(0.95);
+    }
+    45% {
+      transform: rotate(20deg) scale(1.05);
+    }
+    60% {
+      transform: rotate(-20deg) scale(0.95);
+    }
+    75% {
+      transform: rotate(0deg) scale(1.05);
+    }
+    100% {
+      transform: rotate(0deg) scale(1);
+    }
+  }
+
+  .waving-hand {
+    display: inline-block;
+    animation: wave 4s linear infinite;
+    margin-right: 0.5rem;
+  }
+
+  .section-bar {
+    background-color: var(--secondary);
+    padding: 0.5rem 2rem;
+    border-radius: 1rem;
+    color: var(--primary);
+    font-weight: 700;
+    font-size: 1.15rem;
+    margin-bottom: 1.5rem;
+  }
+`;
--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,5 +1,5 @@
+import React from 'react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
-
 import Container from './styles';
 import ProjectModule from '../ProjectModule';
 
@@ -11,6 +11,79 @@ import bakoads from '../../images/projectThumbs/bakoads.png';
 import mariaVitoria from '../../images/projectThumbs/mariaVitoria.png';
 import ofcBordados from '../../images/projectThumbs/ofcBordados.png';
 
+const projects = [
+  {
+    projectName: 'Dashboard para Anúncios',
+    subtitle:
+      'A pedido da empresa de anúncios, foi feita uma plataforma de controle de seus clientes e suas informações, e devolver os dados dos anúncios em um dashboard.',
+    skills: ['React', 'Express', 'Postgres'],
+    imageProject: bakoads,
+    access: 'Bakoads Anúncios',
+    linkProject: 'https://github.com/Wallace027Dev/loran-m-rosa',
+    linkSite: 'https://bakoads.vercel.app',
+  },
+  {
+    projectName: 'Site Institucional',
+    subtitle:
+      'Site Institucional para a Oficina de Bordados. Feito para uma empresa com vários anos de mercado em todo o Espírito Santo.',
+    skills: ['React JS', 'React Router', 'Styled-comp'],
+    imageProject: ofcBordados,
+    access: 'Oficina de Bordados',
+    linkProject: 'https://github.com/Wallace027Dev/ofc-site',
+    linkSite: 'https://ofcbordados.com.br',
+  },
+  {
+    projectName: 'Plataforma de Cursos',
+    subtitle:
+      'Projeto final da Staart, onde a proposta era fazer uma plataforma de cursos, criando um design do zero e consumindo uma API com as informações dos cursos.',
+    skills: ['React JS', 'Axios', 'Styled-comp'],
+    imageProject: staartClone,
+    access: 'Staart Clone',
+    linkProject: 'https://github.com/Wallace027Dev/staart-clone',
+    linkSite: 'https://staart-clone.vercel.app',
+  },
+  {
+    projectName: 'Portifólio - Maria Vitória',
+    subtitle:
+      'Portifólio feito para a artista Maria Vitória. Foram feitas 3 divisões, para os tipos das artes apresentadas, e colocado links para contactá-la.',
+    skills: ['React JS', 'React Router', 'Styled-comp'],
+    imageProject: mariaVitoria,
+    access: 'Portifólio - Maria Vitória',
+    linkProject: 'https://github.com/Wallace027Dev/portfolio-mavi',
+    linkSite: 'https://mariafranca.vercel.app',
+  },
+  {
+    projectName: 'Autenticação',
+    subtitle: 'Nesse projeto foi usado React, Routers e Firebase.',
+    skills: ['Firebase', 'React JS', 'Javascript'],
+    imageProject: authStorage,
+    access: 'Autenticação',
+    linkSite: 'https://autenticacao-e-armazenamento-react.vercel.app/login',
+    linkProject:
+      'https://github.com/Wallace027Dev/Autenticacao-armazenamento-dados-react',
+  },
+  {
+    projectName: 'Lista de Tarefas',
+    subtitle:
+      'Uma lista de tarefas desenvolvida com JS puro, manipulando o DOM e Eventos().',
+    skills: ['Javascript', 'Babel', 'Tailwind'],
+    imageProject: todoPrint,
+    access: 'To Do List',
+    linkProject: 'https://to-do-olive-six.vercel.app/',
+    linkSite: 'https://to-do-olive-six.vercel.app/',
+  },
+  {
+    projectName: 'Media Social',
+    subtitle:
+      'Projeto de mídia social, onde posts são submetidos, enviados e salvos em uma API.',
+    skills: ['Node', 'React JS', 'Fetch API'],
+    imageProject: postsApi,
+    access: 'Social Media',
+    linkProject: 'https://github.com/Wallace027Dev/Consumo-de-API-React/',
+    linkSite: 'https://consumo-de-api-com-react.vercel.app/',
+  },
+];
+
 export default function Slider() {
   return (
     <Container id="my-projects">
@@ -19,117 +92,33 @@ export default function Slider() {
           type: 'loop',
           autoplay: true,
           interval: 2500,
-          perPage: 3, // Para pc
+          perPage: 3,
           breakpoints: {
             1440: {
-              perPage: 2, // Para notebook
+              perPage: 2,
             },
             1024: {
-              perPage: 1, // Para tablet
+              perPage: 1,
             },
           },
         }}
       >
-        <SplideSlide>
-          <ProjectModule
-            className="carrossel-item"
-            projectName="Dashboard para Anúncios"
-            subtitle="A pedido da empresa de anúncios, foi feita uma plataforma de controle de seus clientes e suas informações, e devolver os dados dos anúncios em um dashboard."
-            skill1="React"
-            skill2="Express"
-            skill3="Postgres"
-            imageProject={bakoads}
-            acess="Bakoads Anúncios"
-            linkProject="https://github.com/Wallace027Dev/loran-m-rosa"
-            linkSite="https://bakoads.vercel.app"
-          />
-        </SplideSlide>
-        <SplideSlide>
-          <ProjectModule
-            className="carrossel-item"
-            projectName="Site Institucional"
-            subtitle="Site Institucional para a Oficina de Bordados. Feito para uma empresa com vários anos de mercado em todo o Espírito Santo."
-            skill1="React JS"
-            skill2="React Router"
-            skill3="Styled-comp"
-            imageProject={ofcBordados}
-            acess="Oficina de Bordados"
-            linkProject="https://github.com/Wallace027Dev/ofc-site"
-            linkSite="https://ofcbordados.com.br"
-          />
-        </SplideSlide>
-        <SplideSlide>
-          <ProjectModule
-            className="carrossel-item"
-            projectName="Plataforma de Cursos"
-            subtitle="Projeto final da Staart, onde a proposta era fazer uma plataforma de cursos, 
-            criando um design do zero e consumindo uma api com as informações dos cursos.."
-            skill1="React JS"
-            skill2="Axios"
-            skill3="Styled-comp"
-            imageProject={staartClone}
-            acess="Staart Clone"
-            linkProject="https://github.com/Wallace027Dev/staart-clone"
-            linkSite="https://staart-clone.vercel.app"
-          />
-        </SplideSlide>
-
-        <SplideSlide>
-          <ProjectModule
-            className="carrossel-item"
-            projectName="Portifólio - Maria Vitória"
-            subtitle="Portifólio feito para a artista Maria Vitória. Foram feitas 3 divisões, para os tipos das artes apresentadas, e colocado links para contactá-la."
-            skill1="React JS"
-            skill2="React Router"
-            skill3="Styled-comp"
-            imageProject={mariaVitoria}
-            acess="Portifólio - Maria Vitória"
-            linkProject="https://github.com/Wallace027Dev/portfolio-mavi"
-            linkSite="https://mariafranca.vercel.app"
-          />
-        </SplideSlide>
-        <SplideSlide>
-          <ProjectModule
-            className="carrossel-item"
-            projectName="Autenticação"
-            subtitle="Nesse projeto foi usado React, Routers e Firebase."
-            skill1="Firebase"
-            skill2="React JS"
-            skill3="Javascript"
-            imageProject={authStorage}
-            acess="Autenticação"
-            linkSite="https://autenticacao-e-armazenamento-react.vercel.app/login"
-            linkProject="https://github.com/Wallace027Dev/Autenticacao-armazenamento-dados-react"
-          />
-        </SplideSlide>
-        <SplideSlide>
-          <ProjectModule
-            className="carrossel-item"
-            projectName="Lista de Tarefas"
-            subtitle="Uma lista de tarefas desenvolvida com JS puro, manipulando o DOM e Eventos()."
-            skill1="Javascript"
-            skill2="Babel"
-            skill3="Tailwind"
-            imageProject={todoPrint}
-            acess="To Do List"
-            linkProject="https://to-do-olive-six.vercel.app/"
-            linkSite="https://to-do-olive-six.vercel.app/"
-          />
-        </SplideSlide>
-        <SplideSlide>
-          <ProjectModule
-            className="carrossel-item"
-            projectName="Media Social"
-            subtitle="Projeto de media social, onde posts são sumbetidos, e, enviados e salvos em uma API."
-            skill1="Node"
-            skill2="React JS"
-            skill3="Fetch API"
-            imageProject={postsApi}
-            acess="Social Media"
-            linkProject="https://github.com/Wallace027Dev/Consumo-de-API-React/"
-            linkSite="https://consumo-de-api-com-react.vercel.app/"
-          />
-        </SplideSlide>
+        {projects.map((project, index) => (
+          <SplideSlide key={index}>
+            <ProjectModule
+              className="carrossel-item"
+              projectName={project.projectName}
+              subtitle={project.subtitle}
+              skill1={project.skills[0]}
+              skill2={project.skills[1]}
+              skill3={project.skills[2]}
+              imageProject={project.imageProject}
+              access={project.access}
+              linkProject={project.linkProject}
+              linkSite={project.linkSite}
+            />
+          </SplideSlide>
+        ))}
       </Splide>
     </Container>
   );
